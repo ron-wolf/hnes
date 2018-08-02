663324f81e73f7ca77c83d4990a55e24f501c051
Ron Wolf <ron-wolf@users.noreply.github.com>
2018-08-02 05:19:58 -0400

    Extremely prospective revisions to Cartridge
    Rewrite do-notation for `parse` in Applicative style; factor out INesFileHeader & `parseHeader`

--- src/Emulator/Cartridge.hs
+++ src/Emulator/Cartridge.hs
@@ -4,9 +4,12 @@ module Emulator.Cartridge (
   , parse
 ) where
 
+import           Control.Applicative         ((<$>), (<*>))
+import           Control.Monad               (liftM2)
 import           Control.Monad.ST
 import           Data.Bits
 import qualified Data.ByteString             as BS
+import           Data.Function               (on)
 import           Data.IORef
 import qualified Data.Vector.Unboxed         as VU
 import qualified Data.Vector.Unboxed.Mutable as VUM
@@ -14,14 +17,14 @@ import           Data.Word
 import           Emulator.Util               (sliceBS)
 import           Prelude                     hiding (read)
 
-data INesFileHeader = INesFileHeader {
-  format   :: Word8,
-  numPrg   :: Int,
-  numChr   :: Int,
-  control1 :: Int,
-  control2 :: Int,
-  numRam   :: Int
-} deriving (Eq, Show)
+-- data INesFileHeader = INesFileHeader {
+--   format   :: Word8,
+--   numPrg   :: Int,
+--   numChr   :: Int,
+--   control1 :: Int,
+--   control2 :: Int,
+--   numRam   :: Int
+-- } deriving (Eq, Show)
 
 data Mirror
   = MirrorHorizontal
@@ -39,34 +42,32 @@ data Cartridge = Cartridge {
   mapperType :: Int
 }
 
-parseHeader :: BS.ByteString -> INesFileHeader
-parseHeader bs = INesFileHeader
-  (fromIntegral $ BS.index bs 3)
-  (fromIntegral $ BS.index bs 4)
-  (fromIntegral $ BS.index bs 5)
-  (fromIntegral $ BS.index bs 6)
-  (fromIntegral $ BS.index bs 7)
-  (fromIntegral $ BS.index bs 8)
+-- parseHeader :: BS.ByteString -> INesFileHeader
+-- parseHeader bs = INesFileHeader
+--   (fromIntegral $ BS.index bs 3)
+--   (fromIntegral $ BS.index bs 4)
+--   (fromIntegral $ BS.index bs 5)
+--   (fromIntegral $ BS.index bs 6)
+--   (fromIntegral $ BS.index bs 7)
+--   (fromIntegral $ BS.index bs 8)
 
 parse :: BS.ByteString -> IO Cartridge
-parse bs = do
-  let (INesFileHeader _ numPrg numChr ctrl1 ctrl2 _) = parseHeader bs
-  let prgOffset = numPrg * prgRomSize
-  let prgRom = sliceBS headerSize (headerSize + prgOffset) bs
-  let chrOffset = numChr * chrRomSize
-  let chrRom = if numChr == 0 then (BS.replicate chrRomSize 0)
-               else sliceBS (headerSize + prgOffset) (headerSize + prgOffset + chrOffset) bs
-
-  chr <- VU.thaw $ VU.fromList $ BS.unpack chrRom
-  prg <- VU.thaw $ VU.fromList $ BS.unpack prgRom
-  sram <- VUM.replicate 0x2000 0
-
-  let mirrorV = (ctrl1 .&. 1) .|. (((ctrl1 `shiftR` 3) .&. 1) `shiftR` 1)
-  mirror <- newIORef $ toEnum mirrorV
-
-  let mapper = (ctrl1 `shiftR` 4) .|. ((ctrl2 `shiftR` 4) `shiftL` 4)
-
-  pure $ Cartridge chr prg sram mirror mapper
+parse bs = (liftM2 Cartridge `on` process) chrRom prgRom <*> sram <*> wrap mirrorV <*> pure mapper
+  where
+    process = VU.thaw . VU.fromList . BS.unpack
+    wrap    = newIORef . (toEnum :: Int -> BS.ByteString)
+    
+    let prgOffset = numPrg * prgRomSize
+        chrOffset = numChr * chrRomSize
+    in  prgRom               = on sliceBS (headerSize +            ) 0 prgOffset bs
+        chrRom | numChr /= 0 = on sliceBS (headerSize + prgOffset +) 0 chrOffset bs
+               | otherwise   = BS.replicate chrRomSize 0
+    
+    sram    = VUM.replicate 0x2000 0
+    mirrorV = (ctrl1 .&. 1 .|.) $ ((ctrl1 `shiftR` 3) .&. 1) `shiftR` 1
+    mapper  = (ctrl1 `shiftR` 4) .|. (ctrl2 `shiftR` 4 `shiftL` 4)
+    
+    [numPrg, numChr, ctrl1, ctrl2] = map (BS.index bs) [4..7]
 
 headerSize :: Int
 headerSize = 0x10
